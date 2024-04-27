dd57d3c2b4d1ede6c490fb8ce9004fa227cbc350
André Luis <113619337+AndreLuisALCPS@users.noreply.github.com>
2024-04-27 18:44:50 -0300

    Update Cliente.java

--- sistema_bancário/src/sistema_bancário/Cliente.java
+++ sistema_bancário/src/sistema_bancário/Cliente.java
@@ -1,27 +1,29 @@
 package sistema_bancário;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Cliente extends Thread {
-    private Conta conta;
+    private final String nome;
+    private final Conta conta;
 
-    public Cliente(String nome, double saldoInicial) {
-        this.conta = new Conta(this, saldoInicial);
-        this.setName(nome);
+    public Cliente(String nome, Conta conta) {
+        this.nome = nome;
+        this.conta = conta;
     }
-
     @Override
     public void run() {
         while (conta.getSaldo() > 0) {
-            double valorCompra = Math.random() < 0.5 ? 100 : 200;
-            Loja.lojas[(int) (Math.random() * Loja.lojas.length)].realizarCompra(this, valorCompra);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            int valorCompra = ThreadLocalRandom.current().nextBoolean() ? 100 : 200; 
+            if (conta.getSaldo() >= valorCompra && conta.sacar(valorCompra)) {
+                System.out.println(nome + " realizou uma compra de R$ " + valorCompra + ". Saldo restante: R$ " + conta.getSaldo());
+                synchronized (Banco.lojas) {
+                    int numberOfLojas = Banco.lojas.size();
+                    if (numberOfLojas > 0) {
+                        Loja loja = Banco.lojas.get(ThreadLocalRandom.current().nextInt(numberOfLojas));
+                        loja.receberPagamento(valorCompra, nome);
+                    }
+                }
             }
         }
-    }
-
-    public Conta getConta() {
-        return conta;
     }
 }
